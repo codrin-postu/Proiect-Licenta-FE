4738c4031f7e69b522c4ef9ce319680e5b823f43
Codrin <53770525+codrin-postu@users.noreply.github.com>
2022-04-10 14:33:09 +0300

    Project initialization

---
+++ react-app/api-server/app.py
@@ -0,0 +1,14 @@
+import time
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/time')
+def get_current_time():  # put application's code here
+    return {'time': time.time()}
+
+
+if __name__ == '__main__':
+    app.run()
